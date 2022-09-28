405e93fa8be77639aeeb45ffc5fd5648238b2590
jenya228 <borisovjenya33@gmail.com>
2022-09-28 21:25:14 +0300

    asd

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import random as rnd
 
 N = int(input("Введите количество чисел:"))
+choise = int(input("Какую сортировку выбираете? 1 - по возрастанию 2 - по убыванию\n"))
+while choise != 1 and choise !=2:
+    choise = int(input("Какую сортировку выбираете? 1 - по возрастанию 2 - по убыванию\n"))
+
 a = []
 for i in range(N):
     a.append(rnd.randint(1, 99))
 print(a)
 
-for i in range(N - 1):
-    for j in range(N - i - 1):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
+if(choise == 1):
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+else:
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] < a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
 
 print(a)
